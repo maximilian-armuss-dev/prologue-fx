c91d7471e23df93d34026b0f48016e1eac6f1df5
maxi <maximilian.armuss@live.de>
2025-09-16 17:51:36 +0200

    + Tidied up redundant files + Moved from per-sample processing to buffer-based processing + Added linter for VSCode-based IDEs

--- fx/Panner/src/PannerFX.cpp
+++ fx/Panner/src/PannerFX.cpp
@@ -13,14 +13,20 @@ void PannerFX::set_speed(const float speed) { pan_ = speed; }
 
 void PannerFX::set_depth(const float depth) { gain_ = 2 * depth; }
 
-float PannerFX::process_main_L(const float x) {
-    float amped_signal = x * gain_;
-    float panned_signal = amped_signal * pan_;
-    return clip1m1f(panned_signal);
+bool PannerFX::process_main_L(float* main_frames_L, uint32_t frames) {
+    for (uint32_t i = 0; i < frames; ++i) {
+        float amped_signal = main_frames_L[i] * gain_;
+        float panned_signal = amped_signal * pan_;
+        main_frames_L[i] = clip1m1f(panned_signal);
+    }
+    return true;
 }
 
-float PannerFX::process_main_R(const float x) {
-    float amped_signal = x * gain_;
-    float panned_signal = amped_signal * (1 - pan_);
-    return clip1m1f(panned_signal);
+bool PannerFX::process_main_R(float* main_frames_R, uint32_t frames) {
+    for (uint32_t i = 0; i < frames; ++i) {
+        float amped_signal = main_frames_R[i] * gain_;
+        float panned_signal = amped_signal * (1 - pan_);
+        main_frames_R[i] = clip1m1f(panned_signal);
+    }
+    return true;
 }
--- shared/FXBase.hpp
+++ shared/FXBase.hpp
@@ -7,8 +7,8 @@ public:
     virtual void reset() = 0;
     virtual void set_speed(float speed) = 0;
     virtual void set_depth(float depth) = 0;
-    virtual float process_main_L(float x) = 0;
-    virtual float process_main_R(float x) = 0;
-    virtual float process_sub_R(const float x) { return process_main_R(x); }
-    virtual float process_sub_L(const float x) { return process_main_L(x); }
+    virtual bool process_main_L(float* main_frames_L, uint32_t frames) = 0;
+    virtual bool process_main_R(float* main_frames_R, uint32_t frames) = 0;
+    virtual bool process_sub_L(float* sub_frames_L, uint32_t frames) { return process_main_L(sub_frames_L, frames); }
+    virtual bool process_sub_R(float* sub_frames_R, uint32_t frames) { return process_main_R(sub_frames_R, frames); }
 };
